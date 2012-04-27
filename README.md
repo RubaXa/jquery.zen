1037e9c3f6fcee5b706f1cfe55ca7e19272c1093
RubaXa <ibnRubaXa@gmail.com>
2012-04-27 19:49:40 +0400

    :]

---
+++ README.md
@@ -0,0 +1,138 @@
+# jquery.zen :]
+
+
+## Old school
+```js
+$('.menu')
+	.addClass('ready')
+	.on('click', '.item', function (){
+		$(this)
+			.closest('.menu')
+				.children('.selected')
+					.removeClass('selected')
+					.end()
+				.end()
+			.addClass('selected')
+		;
+	})
+	.find('.item')
+		.each(function (){ /* each */ })
+		.css({ cursor: 'pointer' })
+		.filter('.selected')
+			.attr({ title: 'filtred' })
+			.end()
+		.end()
+	.click(function (){ /* click */ })
+	.on('keyup', 'input', function (){ /* keyup */ })
+	.find('textarea').val('').end()
+	.find('.username').html('%username%')
+;
+```
+
+
+## Zen school
+```js
+$.zen('.menu', {
+	'+class': 'ready', // .addClass()
+	'.item': { // .find()
+		'&click': function (){ // .delegate()
+			$.zen(this, {
+				'^.menu': { // .closest()
+					'*.selected': { // .children()
+						'-class': 'selected' // .removeClass()
+					}
+				},
+				'+class': 'selected' // .addClass()
+			});
+		},
+		'': function (){ /* each */ }
+		css: { cursor: 'pointer' },
+		'*.selected': { // .filter()
+			attr: { title: 'filtred' }
+		}
+	},
+	'click': function (){ /* click */ },
+	'&keyup input': function (){ /* keyup */ },
+	'textarea': { '=': '' }
+	'.username': { '=': '%username%' }
+});
+```
+
+
+## Zen
+
+### .find
+```js
+	$('.items').z({
+		'.sub-items': function (){
+			/* each */
+		}
+	});
+
+	$.z('.items', {
+		'.sub-items': {
+			'': function (){ /* each */ },
+			css: { color: 'red' },
+			'-attr': 'disabled'
+		}
+	});
+```
+
+
+### .children
+```js
+	$.z({
+		ul: {
+			'>': function (){ /* each children */ },
+			'>.selected': '-' // remove selected children
+		}
+	})
+```
+
+
+### .filter
+```js
+	$.z({
+		ul: {
+			li: {
+				'+class': 'item',
+				'*:first': { '+class': 'first-item' }
+			}
+		}
+	});
+```
+
+
+### .parent/.closest
+```js
+	// .parent
+	$.z('li', {
+		'^': { '+class': 'is-parent' }
+	});
+
+
+	// .closest
+	$.z({
+		'&click .item': function (){
+			$.z(this, {
+				'^.menu': {
+					'.selected': { '-class': 'selected' }
+				},
+				'+class': 'selected'
+			})
+		}
+	})
+```
+
+
+
+### .append
+```js
+	$.z('.items', { '+': '#id' });
+	$.z('.items', { '+': ['#id', '.sel', { '#id: { '+class': 'sel' } } });
+	$.z('.items', { '+': { '#id: { '+class': 'sel' } } });
+```
+
+
+
+### Я идиот, убейте меня кто-нибудь!!!1
---
+++ jquery.zen.js
@@ -0,0 +1,190 @@
+/**
+ * @param	{document}	document
+ * @param	{jQuery}	$
+ * @param				[undef]
+ */
+(function (document, $, undef){
+	'use strict';
+
+	var
+		  toStr = ({}).toString
+		, $dummy = $()
+		, $document = $(document)
+	;
+
+	var zen = function ($ctx, obj){
+		if( obj === undef ){
+			obj		= $ctx;
+			$ctx	= $document;
+		}
+		else {
+			$ctx	= $ctx.jquery ? $ctx : $($ctx);
+		}
+
+
+		if( obj === '-' ){
+			// .remove();
+			$ctx.remove();
+		}
+		else if( typeof obj === 'function' ){
+			$ctx.each(obj);
+		}
+		else {
+			var key, first, val, name;
+
+			for( key in obj ){
+				val = obj[key];
+				first = key.charAt(0);
+				name = key.substr(1);
+
+				switch( first ){
+					case '': $ctx.each(val); break;
+
+					case '#': // $('#...')
+							zen($(document.getElementById(name)), val);
+						break;
+
+					case '^':
+							if( name === '' )
+								zen($ctx.parent(), val);
+							else
+								zen($ctx.closest(name), val);
+						break;
+
+					case '>': // .children(...)
+					case '*': // .each/filter(...);
+							zenFind(first, $ctx, name, val);
+						break;
+
+					case '-':
+					case '+':
+							zenToggle(first, $ctx, name, val);
+						break;
+
+					case '&': // delegate
+							$('body').delegate($ctx.selector, name, val);
+						break;
+
+					case '=':
+							$ctx.each(function (){
+								$dummy[0] = this;
+								$dummy.length = 1;
+								$dummy['form' in this ? 'val' : 'html'](val);
+							});
+						break;
+
+					default:
+							if( key in $ctx ){
+								// .methods(args)
+								zenApply($ctx, key, val);
+							}
+							else {
+								// .find()
+								zen($ctx.find(key), val);
+							}
+						break;
+				}
+			}
+		}
+
+		return	$ctx;
+	}
+
+
+	var _append = {
+		'+': 'append',
+		'+>': 'appendTo',
+		'+!': 'prependTo',
+		'+^': 'insertBefore'
+	};
+
+
+	function zenToggle(mod, $ctx, name, val){
+		switch( name ){
+			case 'attr':
+					zenApply($ctx, mod === '+' ? 'attr' : 'removeAttr', val);
+				break;
+
+			case 'class': // .addClass/removeClass(...)
+					zenApply($ctx, mod === '+' ? 'addClass' : 'removeClass', val);
+				break;
+
+			case '*': // .prependTo
+					zenApply($ctx, 'prependTo', val, true);
+				break;
+
+			case '>': // .appendTo
+					zenApply($ctx, 'appendTo', val, true);
+				break;
+
+			case '': // .append(args);
+					zenApply($ctx, 'append', val, true);
+				break;
+
+			default: // .bind/unbind(...)
+					zenApply($ctx, mod === '+' ? 'bind' : 'unbind', [name, val]);
+				break;
+		}
+	}
+
+
+	function zenApply($ctx, method, args, zenArgs){
+		if( zenArgs === true ){
+			args = zenApplyArgs(args);
+		}
+
+		if( args === undef || args === null || args.constructor !== Array ){
+			$ctx[method](args);
+		}
+		else {
+			$ctx[method].apply($ctx, args);
+		}
+	}
+
+
+	function zenApplyArgs(args){
+		var ret = [], key, n;
+
+		if( '0' in args ){
+			for( key = 0, n = args.length; key < n; key += 1){
+				if( typeof args[key] === 'string' ){
+					ret.push($(args[key]));
+				} else {
+					ret = ret.concat(zenApplyArgs(args[key]));
+				}
+			}
+		}
+		else {
+			for( key in args ){
+				ret.push( zen(key, args[key]) );
+			}
+		}
+
+		return	ret;
+	}
+
+
+	function zenFind(mod, $ctx, sel, val){
+		if( sel === '' ){
+			sel	= undef
+		}
+
+		if( mod == '>' ){
+			zen($ctx.children(sel), val);
+		}
+		else if( sel === undef ){
+			zen($ctx, val);
+		}
+		else {
+			zen($ctx.find(sel), val);
+		}
+	}
+
+
+	// @export
+	$.zen = $.z = zen;
+	$.fn.zen = $.fn.z = function (obj){
+		zen(this, obj);
+		return	this;
+	};
+})(document, window.jQuery);
